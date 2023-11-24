45a1830a06db695ba6c419ad0c500486602e8491
Victor Gil <victor.e.g.b2@gmail.com>
2023-11-23 20:51:46 -0500

    Adding of body parser, and structure of the array to handle the post

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+let posts = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", res.locals={posts: posts});
    }); 
+
+app.post("/submit", (req, res) => {
+  console.log(req.body["postTitle"]);
+  console.log(req.body["textarea"]);
+  const fecha = new Date();
+  posts.unshift({"title": req.body["postTitle"], "body":req.body["textarea"], "date": fecha.toDateString()});
+  console.log(posts);
+  res.redirect("/");
   
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  }); 
+});
+  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+}); 
